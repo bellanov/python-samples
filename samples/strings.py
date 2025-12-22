5c413183cc839b5c066c66056f71813fd9ebaad6
charlestone <cjmayenga@aggies.ncat.edu>
2025-12-22 15:22:04 -0500

    Add Lab:Python strings

--- samples/strings.py
+++ samples/strings.py
@@ -37,3 +37,41 @@ print(replace_string)  # Output: Hello, Python!
 # Splitting a string
 words = string2.split(" ")  # Split string into a list of words
 print(words)  # Output: ['Python', 'Programming']
+
+# LAB: Coding with Python Strings
+
+#! /usr/bin/python3
+import sys
+sys.version_info[0]
+
+lab_exercise = "Count"
+lab_type = "lab-code"
+python_version = ("%s.%s.%s" % (sys.version_info[0], sys.version_info[1], sys.version_info[2]))
+print("Exercise: %s" % (lab_exercise))
+print("Type: %s" % (lab_type))
+print("Python: %s\n" % (python_version))
+
+#====================================
+
+data = "cloudacademy.python.2019"
+letter1 = 'a'
+word1 = 'cloud'
+num1 = '2019'
+
+#CODE1: Count occurrences of letters and words
+print(f"Count '{letter1}' = {data.count(letter1)}")
+print(f"count '{word1}' = {data.count(word1)}")
+print(f"count '{num1}' = {data.count(num1)}")
+print()
+
+#CODE2: Get length of string
+print(len(data))
+print()
+
+#CODE3: Get max of string - the highest char in the string
+print(f" Max of string: {max(data)}")
+print()
+
+#CODE4: Get min of string - the lowest char in the string
+print(f" Min of string: {min(data)}")
+print()
